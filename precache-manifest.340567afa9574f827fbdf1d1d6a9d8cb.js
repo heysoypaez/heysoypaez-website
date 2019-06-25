82d6fc40811fe5f28ca4260c6d66c236fb517a11
Daniel Páez <danielpaezsw66@gmail.com>
2019-06-25 19:04:57 -0400

    Updates

---
+++ precache-manifest.340567afa9574f827fbdf1d1d6a9d8cb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026d42b6be296caefb7158ecf73a9d95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1f408fea44b09a846d7",
+    "url": "/static/css/main.9287081b.chunk.css"
+  },
+  {
+    "revision": "018a4e880073bad3d167",
+    "url": "/static/js/2.c648bf82.chunk.js"
+  },
+  {
+    "revision": "a1f408fea44b09a846d7",
+    "url": "/static/js/main.e70b8f2d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2c6616219c760a467947bc03e892b799",
+    "url": "/static/media/calculadona-home-view.2c661621.png"
+  },
+  {
+    "revision": "f5632fb1bd52afea469366a28f25d7ef",
+    "url": "/static/media/check-mark.f5632fb1.svg"
+  },
+  {
+    "revision": "0412efafc51c306c66d90e2478dab3e3",
+    "url": "/static/media/envelope.0412efaf.svg"
+  },
+  {
+    "revision": "dec21930a1a4f59520b5c55bff21dc6f",
+    "url": "/static/media/github.dec21930.svg"
+  },
+  {
+    "revision": "1369fdae5c6f85849bdba6802a629435",
+    "url": "/static/media/icon-menu.1369fdae.svg"
+  },
+  {
+    "revision": "f9a248e517e1724cba3c71b4f44f7d0f",
+    "url": "/static/media/linkedin-logo.f9a248e5.svg"
+  },
+  {
+    "revision": "1ab824774b1dcd361df2f2d4a6f56529",
+    "url": "/static/media/patytime-home-view.1ab82477.png"
+  },
+  {
+    "revision": "f1e5751b3bd7c2a4350a9cb7b206c314",
+    "url": "/static/media/platzi-video-home-view.f1e5751b.png"
+  },
+  {
+    "revision": "557e4a2af4b8b9b71dfd04886486fe53",
+    "url": "/static/media/platzi-video-react-native.557e4a2a.png"
+  },
+  {
+    "revision": "b509ba3efddd44158c370d4303788583",
+    "url": "/static/media/platzi.b509ba3e.png"
+  },
+  {
+    "revision": "447540e107267c8406e68fcd09c780ec",
+    "url": "/static/media/twitter.447540e1.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/heysoypaez-website/precache-manifest.1433e37aa4ad765b30060143ae8f6dd4.js"
+  "/precache-manifest.340567afa9574f827fbdf1d1d6a9d8cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/heysoypaez-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
